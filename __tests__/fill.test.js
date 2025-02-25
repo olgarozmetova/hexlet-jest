f03a1e44545b37f429fb2987b7c626f90877f0bc
olga rozmetova <olga.rozmetova@gmail.com>
2025-02-25 11:28:50 +0100

    Update fill.test.js

--- __tests__/fill.test.js
+++ __tests__/fill.test.js
@@ -1,26 +1,26 @@
 import fill from '../src/fill.js';
 
-test('fill', () => {
+test('should work with an empty array', () => {
   let array = [];
   expect(fill(array, '*')).toEqual([]);
 });
 
-test('fill2', () => {
+test('should fill array from start to end index', () => {
   let array = [1, 2, 3, 4];
   expect(fill(array, '*', 1, 3)).toEqual([1, '*', '*', 4]);
 });
 
-test('fill3', () => {
+test('should fill entire array if no start and end are specified', () => {
   let array = [1, 2, 3, 4];
   expect(fill(array, '*')).toEqual(['*', '*', '*', '*']);
 });
 
-test('fill4', () => {
+test('should not fill any element if start is greater than end', () => {
   let array = [1, 2, 3, 4];
   expect(fill(array, '*', 4)).toEqual([1, 2, 3, 4]);
 });
 
-test('fill5', () => {
+test('should fill from start index to the end of the array if end is greater than the array length', () => {
   let array = [1, 2, 3, 4];
   expect(fill(array, '*', 0, 10)).toEqual(['*', '*', '*', '*']);
 });
